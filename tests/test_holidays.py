bdb9c2b95a9cb6cfbf90c391f8062b56ff7ea3f1
inokappa <inokara@gmail.com>
2018-04-15 00:58:56 +0900

    Python 3.6 以外のバージョンでテストがコケるので, S3 から返ってくるデータをソートするようにテストを変更した

--- tests/test_holidays.py
+++ tests/test_holidays.py
@@ -1,29 +1,31 @@
 import os
 import unittest
+import json
 import mock
 from moto import mock_s3
 import requests
 import boto3
 import holidays
 
+
 class HolidaysPyTest(unittest.TestCase):
-   @classmethod
-   def setUp(self):
-       os.environ["BUCKET_NAME"] = "holiday-py"
+    @classmethod
+    def setUp(self):
+        os.environ["BUCKET_NAME"] = "holiday-py"
 
-   @mock.patch('requests.get')
-   def test_get_holiday_csv(self, mock_get):
-       res = requests.Response()
-       res.status_code = 200
-       res._content = ''
-       mock_get.return_value = res
-       self.assertEqual(holidays.getHolidayCsv(), '')
+    @mock.patch('requests.get')
+    def test_get_holiday_csv(self, mock_get):
+        res = requests.Response()
+        res.status_code = 200
+        res._content = ''
+        mock_get.return_value = res
+        self.assertEqual(holidays.getHolidayCsv(), '')
 
-   def test_decode_content(self):
-       self.assertEqual(holidays.decodeContent(b'\x82\xa0'), 'あ')
+    def test_decode_content(self):
+        self.assertEqual(holidays.decodeContent(b'\x82\xa0'), 'あ')
 
-   def test_convert_dict(self):
-       contents = '''国民の祝日・休日月日,国民の祝日・休日名称
+    def test_convert_dict(self):
+        contents = '''国民の祝日・休日月日,国民の祝日・休日名称
 2017-01-01,元日
 2017-01-02,休日
 2017-01-09,成人の日
@@ -31,57 +33,61 @@ class HolidaysPyTest(unittest.TestCase):
 2019-01-01,元日
 2019-01-14,成人の日
 '''
-       data = {'2017-01-01': '元日',
-               '2017-01-02': '振替休日',
-               '2017-01-09': '成人の日',
-               '2018-12-24': '振替休日',
-               '2019-01-01': '元日',
-               '2019-01-14': '成人の日'}
-       self.assertEqual(holidays.convertDict(contents), data)
+        data = {'2017-01-01': '元日',
+                '2017-01-02': '振替休日',
+                '2017-01-09': '成人の日',
+                '2018-12-24': '振替休日',
+                '2019-01-01': '元日',
+                '2019-01-14': '成人の日'}
+        self.assertEqual(holidays.convertDict(contents), data)
 
-   def test_get_years(self):
-       contents = {'2017-01-01': '元日',
-                   '2017-01-02': '振替休日',
-                   '2017-01-09': '成人の日',
-                   '2018-12-24': '振替休日',
-                   '2019-01-01': '元日',
-                   '2019-01-14': '成人の日'}
-       data = ['2017', '2018', '2019']
-       self.assertListEqual(sorted(holidays.getYears(contents)), data)
+    def test_get_years(self):
+        contents = {'2017-01-01': '元日',
+                    '2017-01-02': '振替休日',
+                    '2017-01-09': '成人の日',
+                    '2018-12-24': '振替休日',
+                    '2019-01-01': '元日',
+                    '2019-01-14': '成人の日'}
+        data = ['2017', '2018', '2019']
+        self.assertListEqual(sorted(holidays.getYears(contents)), data)
 
+    @mock_s3
+    def test_save_years_data(self):
+        s3 = boto3.resource('s3', region_name='ap-northeast-1')
+        s3.create_bucket(Bucket=os.getenv('BUCKET_NAME'))
 
-   @mock_s3
-   def test_save_years_data(self):
-       s3 = boto3.resource('s3', region_name='ap-northeast-1')
-       s3.create_bucket(Bucket=os.getenv('BUCKET_NAME'))
+        contents = {'2017-01-01': '元日',
+                    '2018-12-24': '振替休日',
+                    '2019-01-14': '成人の日'}
+        holidays.saveYearsData(contents)
 
-       contents = {'2017-01-01': '元日',
-                   '2018-12-24': '振替休日',
-                   '2019-01-14': '成人の日'}
-       holidays.saveYearsData(contents)
+        data = '{"2017-01-01": "元日"}'
+        body = s3.Object('holiday-py', '2017/data.json').get()['Body'].read().decode("utf-8")
+        self.assertEqual(body, data)
 
-       data = '{"2017-01-01": "元日"}'
-       body = s3.Object('holiday-py', '2017/data.json').get()['Body'].read().decode("utf-8")
-       self.assertEqual(body, data)
+        data = '{"2018-12-24": "振替休日"}'
+        body = s3.Object('holiday-py', '2018/data.json').get()['Body'].read().decode("utf-8")
+        self.assertEqual(body, data)
 
-       data = '{"2018-12-24": "振替休日"}'
-       body = s3.Object('holiday-py', '2018/data.json').get()['Body'].read().decode("utf-8")
-       self.assertEqual(body, data)
+        data = '{"2019-01-14": "成人の日"}'
+        body = s3.Object('holiday-py', '2019/data.json').get()['Body'].read().decode("utf-8")
+        self.assertEqual(body, data)
 
-       data = '{"2019-01-14": "成人の日"}'
-       body = s3.Object('holiday-py', '2019/data.json').get()['Body'].read().decode("utf-8")
-       self.assertEqual(body, data)
+    @mock_s3
+    def test_save_all_data(self):
+        s3 = boto3.resource('s3', region_name='ap-northeast-1')
+        s3.create_bucket(Bucket=os.getenv('BUCKET_NAME'))
 
-   @mock_s3
-   def test_save_all_data(self):
-       s3 = boto3.resource('s3', region_name='ap-northeast-1')
-       s3.create_bucket(Bucket=os.getenv('BUCKET_NAME'))
+        contents = {'2017-01-01': '元日',
+                    '2018-12-24': '振替休日',
+                    '2019-01-14': '成人の日'}
+        holidays.saveAllData(contents)
 
-       contents = {'2017-01-01': '元日',
-                   '2018-12-24': '振替休日',
-                   '2019-01-14': '成人の日'}
-       holidays.saveAllData(contents)
+        data = '{"2017-01-01": "元日", "2018-12-24": "振替休日", "2019-01-14": "成人の日"}'
+        body = s3.Object('holiday-py', 'data.json').get()['Body'].read().decode("utf-8")
+        sorted_body = {}
+        for k, v in sorted(json.loads(body).items(), key=lambda x: x[0]):
+            sorted_body[k] = v
+        json_body = json.dumps(sorted_body, ensure_ascii=False)
 
-       data = '{"2017-01-01": "元日", "2018-12-24": "振替休日", "2019-01-14": "成人の日"}'
-       body = s3.Object('holiday-py', 'data.json').get()['Body'].read().decode("utf-8")
-       self.assertEqual(body, data)
+        self.assertEqual(json_body, data)
