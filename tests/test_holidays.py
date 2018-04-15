84d9b3db15a7b2bb4cea2aaa352fdefb8db8e49d
inokappa <inokara@gmail.com>
2018-04-15 17:53:54 +0900

    Add putObject test

--- tests/test_holidays.py
+++ tests/test_holidays.py
@@ -90,3 +90,21 @@ class HolidaysPyTest(unittest.TestCase):
             sorted_body[k] = v
 
         self.assertDictEqual(sorted_body, json.loads(data))
+
+    @mock_s3
+    def test_put_object(self):
+        s3 = boto3.resource('s3', region_name='ap-northeast-1')
+        s3.create_bucket(Bucket=os.getenv('BUCKET_NAME'))
+
+        contents = {'2017-01-01': '元日',
+                    '2018-12-24': '振替休日',
+                    '2019-01-14': '成人の日'}
+        holidays.putObject(json.dumps(contents), 'data.json')
+
+        data = '{"2017-01-01": "元日", "2018-12-24": "振替休日", "2019-01-14": "成人の日"}'
+        body = s3.Object('holiday-py', 'data.json').get()['Body'].read().decode("utf-8")
+        sorted_body = {}
+        for k, v in sorted(json.loads(body).items(), key=lambda x: x[0]):
+            sorted_body[k] = v
+
+        self.assertDictEqual(sorted_body, json.loads(data))
